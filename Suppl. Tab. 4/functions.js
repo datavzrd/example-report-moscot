3ea80740c1de6ace4c0e521a6de83ea6aec237da
fxwiegand <fxwiegand@wgdnet.de>
2025-02-06 13:57:22 +0100

    Deploy report

--- Suppl. Tab. 4/functions.js
+++ Suppl. Tab. 4/functions.js
@@ -1,14 +1,3 @@
-function custom_func_5e02566f3c5f43d78937a1307329f345(value) { try { 
-  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
-  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
-  if (value === "Yes") {
-      return trueLabel;
-  } else if (value === "No") {
-      return falseLabel;
-  } else {
-      return value;
-  }
- } catch (e) { datavzrd.custom_error(e, 'Highly-Variable') }}
 function custom_func_26eca094555c6cb048a47d76a23917ae(value) { try { 
     const pmidRegex = /PMID:\s*(\d+)/;
     const match = value.match(pmidRegex);
@@ -20,3 +9,14 @@ function custom_func_26eca094555c6cb048a47d76a23917ae(value) { try {
         return value;  // Return the original value if no valid PMID is found
     }
  } catch (e) { datavzrd.custom_error(e, 'PMID') }}
+function custom_func_5e02566f3c5f43d78937a1307329f345(value) { try { 
+  const trueLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(31, 119, 180); border-radius: 0.4em; text-align: center;">+</span>';
+  const falseLabel = '<span style="display: inline-block; margin: 1px; width: 2em; height: 2em; line-height: 2em; font-size: 1em; font-weight: bold; color: white; background-color: rgb(214, 39, 40); border-radius: 0.4em; text-align: center;">-</span>';
+  if (value === "Yes") {
+      return trueLabel;
+  } else if (value === "No") {
+      return falseLabel;
+  } else {
+      return value;
+  }
+ } catch (e) { datavzrd.custom_error(e, 'Highly-Variable') }}
